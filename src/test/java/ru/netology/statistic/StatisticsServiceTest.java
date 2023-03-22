70948238ecef70cacc75f5798c67aab3a9fdf12b
Stas1092 <ss9262694394@gmail.com>
2023-03-22 13:20:59 +0200

    add test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,15 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    void findSecondMaxIncome() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 50, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 50;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
